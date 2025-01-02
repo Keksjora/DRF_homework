658d67b8b654394a7ea2b6d5dc6cac1dfeb19599
Max Odegov <zoomzooom228@mail.ru>
2025-01-02 22:33:59 +0700

    test8

--- config/settings.py
+++ config/settings.py
@@ -175,8 +175,8 @@ CELERY_BEAT_SCHEDULE = {
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.yandex.ru"
 EMAIL_PORT = os.getenv("EMAIL_PORT")
-EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS").lower() == "true"
-EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL").lower() == "true"
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "true"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "true"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
